584a26546b69cba669d44f73a96e50b049cada50
Luchito-xx <luchoactis16@gmail.com>
2025-08-28 02:27:56 -0300

    Añadido Class Dice: Giro, Obtencion resultado

--- core/dice.py
+++ core/dice.py
@@ -1,2 +1,21 @@
-class dice:
-    pass
+import random
+
+class Dice:
+    def __init__(self):
+        self.__valor__ = 1
+
+    def roll(self):
+        self.__valor__ = random.randint(1, 6)
+        return self.__valor__
+    
+    def __str__(self):
+        return f"Dado: {self.__valor__}" 
+           
+    def get_valor(self):
+        return self.__valor__
+
+if __name__ == "__main__":
+    d = Dice()
+    print(d.roll()) 
+    print(d.get_valor())
+    print(d)
