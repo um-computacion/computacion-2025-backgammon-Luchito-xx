057250c1ed45cfaf15d3debd8a19e53690081959
Luchito-xx <luchoactis16@gmail.com>
2025-08-28 02:17:01 -0300

    Añadido Class Dice: Giro, Obtencion resultado

--- core/player.py
+++ core/player.py
@@ -1,2 +1,23 @@
 class player:
-    pass
+    def __init__(self, name, direccion, fichas=99):
+        self.__name__ = name
+        self.__direccion__ = direccion
+        self.__fichas__ = fichas
+        self.__turno__ = False
+        self.__ganador__ = False
+    
+    def get_name(self):
+        return self.__name__
+    
+    def get_direccion(self):    
+        return self.__direccion__
+    
+    def get_fichas(self):
+        return self.__fichas__
+    
+    def __repr__(self):
+        return f"Player: {self.__name__}, Direccion: {self.__direccion__}, Fichas: {self.__fichas__}, Turno: {self.__turno__}, Ganador: {self.__ganador__}"
+    
+if __name__ == "__main__":
+    p = player("Lucho", -1)
+    print(p)
