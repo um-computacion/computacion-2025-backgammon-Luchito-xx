13ede9f4da1d0f59c186699ab42a70aecdeecda4
Luchito-xx <luchoactis16@gmail.com>
2025-08-29 02:39:48 -0300

    Eliminado de atributos y metodos innecesarios, posible logica de fichas capturadas

--- core/player.py
+++ core/player.py
@@ -1,24 +1,14 @@
 class player:
-    def __init__(self, name, direccion, fichas=99):
+    def __init__(self, name):
         self.__name__ = name
-        self.__direccion__ = direccion
-        self.__fichas__ = fichas
-        self.__turno__ = False
-        self.__ganador__ = False
+        self.__fichas_afuera__ = 0 # falta implementar
     
     def get_name(self):
         return self.__name__
-    
-    def get_direccion(self):    
-        return self.__direccion__
-    
-    def get_fichas(self):
-        return self.__fichas__
-    
+
     def __repr__(self):
-        return f"Player: {self.__name__}, Direccion: {self.__direccion__}, Fichas: {self.__fichas__}, Turno: {self.__turno__}, Ganador: {self.__ganador__}"
-    
+        return f"Player: {self.__name__}"
     
 if __name__ == "__main__":
-    p = player("Lucho", -1)
+    p = player("Lucho")
     print(p)
