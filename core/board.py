b86a6d0b6e92bd0a0931a0175b7a5ff86d3a9ab1
Luchito-xx <luchoactis16@gmail.com>
2025-08-29 02:36:37 -0300

    Mejora: get_board (formato), mover (validaciones y movimiento en base a reglas de juego)

--- core/board.py
+++ core/board.py
@@ -1,36 +1,70 @@
-from exceptions import PosicionInvalidaError
-
 class board:
     def __init__(self):
-        self.__celdas__ = [0] * 24
+        self.__celdas__ = [("X", 0)] * 24
 
     def inicio(self):
-        for i in range(self.__celdas__.__len__()):
-            if i > 0 and i < 6:
-                self.__celdas__[i] = 1
-            elif i > 17 and i < 23:
-                self.__celdas__[i] = 2
-                
+        self.__celdas__[0] = ("A", 2)  
+        self.__celdas__[11] = ("A", 5)
+        self.__celdas__[16] = ("A", 3)
+        self.__celdas__[18] = ("A", 5)
+
+        self.__celdas__[23] = ("B", 2)
+        self.__celdas__[12] = ("B", 5)
+        self.__celdas__[7] = ("B", 3)
+        self.__celdas__[5] = ("B", 5)   
+             
     def __str__(self):
         return f"Board: {self.__celdas__}"
     
-    def get_pos(self, pos):
-        return self.__celdas__[pos]
+    def get_celda(self, celdax:int):
+        return self.__celdas__[celdax]
     
     def get_board(self):
-        return self.__celdas__
+        # Mejorar formato
+        print( "Celda | Fichas | Jugador"  )
+        for i, celda in enumerate(self.__celdas__):
+            print( f"  {i}   |   {celda[1]}   |   {celda[0]} " )
+        return ""
     
-    def mover(self, celda, dado):
+    def mover(self, celda: int, dado: int, jugador:str):
         try:
+
             if celda < 0 or celda > 23:
-                raise PosicionInvalidaError("La celda inicial es inválida.")
-        
-            ficha = self.__celdas__[celda]    
-            if ficha != 0:
-                self.__celdas__[celda] = self.__celdas__[celda+dado]
-                self.__celdas__[celda+dado] = ficha
-                return self.__celdas__
-        except PosicionInvalidaError as e:
+                raise ValueError("La celda inicial es invalida.")
+            
+            #Validar celda origen
+            celda_origen = self.__celdas__[celda]  
+            if celda_origen == None:
+                raise ValueError("No hay ficha en la celda seleccionada.")
+            if celda_origen[0] != jugador:
+                raise ValueError("La celda no pertenece al jugador.")
+
+
+            # Calcular celda destino y validar
+            new_celda = celda + dado if jugador == "A" else celda - dado
+            if new_celda < 0 or new_celda > 23:
+                raise ValueError("Movimiento se va del tablero.")
+            if new_celda != ("X",0) and self.__celdas__[new_celda][1] >= 2  and self.__celdas__[new_celda][0] != jugador:
+                raise ValueError(f"La celda destino esta ocupada por el oponente {self.__celdas__[new_celda][0]}, tiene {self.__celdas__[new_celda][1]} fichas")
+            
+            # Quitar ficha origen
+            if celda_origen[1] <= 1:
+                self.__celdas__[celda] = ("X", 0)
+            else:
+                self.__celdas__[celda] = (jugador, celda_origen[1] - 1)
+
+            # Poner ficha destino
+            if self.__celdas__[new_celda] == ("X", 0):
+                self.__celdas__[new_celda] = (jugador, 1)
+
+            elif self.__celdas__[new_celda][0] == jugador:
+                self.__celdas__[new_celda] = (jugador, self.__celdas__[new_celda][1] + 1)
+
+            else:
+                self.__celdas__[new_celda] = (jugador, 1)
+                # Falta capturar ficha oponente y poner en barra
+
+        except ValueError as e:
             return str(e) 
             
         
@@ -38,6 +72,7 @@ class board:
 if __name__ == "__main__":
     b = board()
     b.inicio()
-    print(b)
-    print(b.mover(1, 3))
-    print(b.mover(1, 1231))
+    print(b, "\n")
+    print(b.get_celda(0), "\n")
+    print(b.mover(0, 5, "A"), "\n")
+    print(b.get_board(), "\n")
