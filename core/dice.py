e6e2455c20de745de32c89808e9d69e7f5be7979
Luchito-xx <luchoactis16@gmail.com>
2025-08-31 22:31:02 -0300

    Implementacion dice devuelve una tupla de dos valores random

--- core/dice.py
+++ core/dice.py
@@ -2,10 +2,10 @@ import random
 
 class Dice:
     def __init__(self):
-        self.__valor__ = 1
+        self.__valor__ = (1,1)
 
     def roll(self):
-        self.__valor__ = random.randint(1, 6)
+        self.__valor__ = [random.randint(1, 6), random.randint(1, 6)]
         return self.__valor__
     
     def __str__(self):
