19929c7118b9341133876366437b7feb783a2b49
Luchito-xx <luchoactis16@gmail.com>
2025-09-13 21:48:49 -0300

    Excepciones pensadas para usar en validaciones.py

---
+++ core/exceptions.py
@@ -0,0 +1,12 @@
+class FueraDeRangoError(Exception):
+    pass
+
+class CeldaInvalidaError(Exception):
+    pass
+
+class ValidacionError(Exception):
+    pass
+
+class CeldaBloqueadaError(Exception):
+    pass
+
