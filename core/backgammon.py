83fbe8e02bec81a5583226c416f29ef516fd398d
Luchito-xx <luchoactis16@gmail.com>
2025-09-13 21:44:46 -0300

    Se comenta manejo planteado para backgammon

--- core/backgammon.py
+++ core/backgammon.py
@@ -1,2 +1,25 @@
-class main:
+class Backgammon:
+    ''' 
+    Controlador del juego
+    
+    Que hace: 
+        - Manejar jugadores, tablero, dados... Con el fin de ejecutar logica del juego
+        - Facilitar manejo para el CLI y algunas pruebas
+
+    Posibles atributos
+        - __board
+        - __player
+        - __turno
+        - __dice
+        - __validacion
+
+    Metodos a aplicar:
+        - constructor __init__
+        - inicio()
+        - roll_dice()
+        - validar_movimiento()
+        - mover()
+        - victoria()
+         
+    '''
     pass
