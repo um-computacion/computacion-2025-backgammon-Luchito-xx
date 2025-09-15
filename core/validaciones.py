ae35787a45fc71e9cbf8e98d151d3945a22d4f37
Luchito-xx <luchoactis16@gmail.com>
2025-09-14 23:03:22 -0300

    Planteo validacion de movimiento se usan @staticmethods, falta implementar en juego

--- core/validaciones.py
+++ core/validaciones.py
@@ -17,7 +17,18 @@ class Validaciones:
     def validar_ficha_celda(ficha:list, jugador:str):
         if not ficha:
             raise CeldaInvalidaError("La celda no tiene fichas en juego")
-        if ficha.get_jugador() != jugador:
+        if ficha != jugador:
             raise CeldaInvalidaError("La celda contiene fichas de otro jugador")
+    @staticmethod
+    def validar_destino(ficha_destino:list , jugador:str):
+        if ficha_destino[0].get_jugador() != jugador and len(ficha_destino) >= 2:
+            raise CeldaBloqueadaError("La celda destino de la ficha esta ocupada por 2 o mas fichas enemigas")
     
-    
+    @staticmethod
+    def movimiento_valido(celdas:list, celda:int, saltos:int, jugador:str)
+        Validaciones.validar_celda(celda)
+        ficha = celdas[celda]
+        Validaciones.validar_ficha_celda(ficha, jugador)
+        destino = celda + saltos if jugador == "X" else celda - saltos 
+        ficha_destino = celdas[celda]
+        Validaciones.validar_destino(ficha_destino, jugador)
