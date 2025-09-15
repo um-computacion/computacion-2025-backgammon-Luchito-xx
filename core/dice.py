59d38336060cc7b2eff9432762cca23bcc3a44a2
Luchito-xx <luchoactis16@gmail.com>
2025-09-14 22:36:02 -0300

    Añadido logica para dados dobles iguales

--- core/dice.py
+++ core/dice.py
@@ -2,12 +2,16 @@ import random
 
 class Dice:
     def __init__(self):
-        self.__valor__ = (1,1)
+        self.__valor__ = [1,1]
 
     def roll(self):
         self.__valor__ = [random.randint(1, 6), random.randint(1, 6)]
-        return self.__valor__
-    
+        if self.__valor__[0] == self.__valor__[1]:
+            self.__valor__ = [self.__valor__[0]] * 4
+            return self.__valor__
+        else: 
+            return self.__valor__
+
     def __str__(self):
         return f"Dado: {self.__valor__}" 
            
