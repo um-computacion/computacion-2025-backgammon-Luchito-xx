ade67b357d3b2b70ecf4c3666a02c51194664bb8
Luchito-xx <luchoactis16@gmail.com>
2025-09-15 17:49:13 -0300

    Modifico def mover (manejo de saltos y movimiento basico) falta usar validaciones. Implemento def cambiar turno

--- core/backgammon.py
+++ core/backgammon.py
@@ -1,6 +1,7 @@
 from board import Board
 from player import Player
 from dice import Dice
+from validaciones import *
 
 ''' 
     Controlador del juego
@@ -32,32 +33,48 @@ class Backgammon:
         self.__players = [Player("X"), Player("O")]
         self.__dice = Dice()
         self.__turno = 0
-        self.__movimientos = []
+        self.__saltos = []
 
     def inicio(self):
         self.__board.inicio()
         self.__turno = 0
-        self.__movimientos = []
+        self.__saltos = []
     
     def tirar_dado(self):
-        self.__movimientos = self.__dice.roll()
-        return self.__movimientos
+        self.__saltos = self.__dice.roll()
+        return self.__saltos
     
     def get_saltos(self):
-        return list(self.__movimientos)
+        return list(self.__saltos)
     
     def get_jugador(self):
         return self.__players[self.__turno]
     
-    def mover(self, celda:int, saltos:int, jugador:str):
+    def mover(self, celda:int, salto:int):
+        
+        if not self.__saltos:
+            raise ValueError("Tirar dados primero")
+        
+        try:
+            i = self.__saltos.index(salto)
+        except ValueError:
+            raise ValueError(f"No hay un salto pendiente igual a {salto}")
+
+        jugador = self.get_jugador()
+
+        self.__board.mover(jugador.get_nombre(), celda, salto)
+
+        self.__saltos.pop(i)
 
         # Como  manejar movimientos?
+
         # cambiar turno cuando no queden mas
 
         pass
 
     def cambio_turno(self):
-        pass
+        self.__turno = 1 - self.__turno
+        self.__saltos = []
 
 
 
