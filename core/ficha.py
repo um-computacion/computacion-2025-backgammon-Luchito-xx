bfce60ca398caee28c434f25b103a8abfbe859d2
Luchito-xx <luchoactis16@gmail.com>
2025-09-16 19:07:40 -0300

    Cambio minimo: dueño -> jugador

--- core/ficha.py
+++ core/ficha.py
@@ -1,34 +1,35 @@
 class Ficha:
 
-    def __init__(self, dueño: str):
-        self.__dueño__ = dueño
-        self.__capturada__ = False
+    def __init__(self, jugador: str):
+        self.__jugador = jugador
+        self.__capturada = False
 
-    def get_dueño(self):
-        return self.__dueño__
+    def get_jugador(self):
+        return self.__jugador
 
-    def set_dueño(self, dueño):
-        self.__dueño__ = dueño
+    def set_jugador(self, jugador):
+        self.__jugador = jugador
     
     def get_capturada(self):
-        return self.__capturada__
+        return self.__capturada
     
     def set_capturada(self, capturada: bool):
-        self.__capturada__ = capturada
+        self.__capturada = capturada
     
     def __repr__(self) -> str:
-        estado = "Capturada" if self.__capturada__ else "Libre"
-        return f"Dueño de la ficha es {self.__dueño__} y esta {estado}"
+        estado = "Capturada" if self.__capturada else "Libre"
+        return f"Dueño de la ficha es {self.__jugador} y esta {estado}"
 
 if __name__ == "__main__":
     f = Ficha("X")
     g = Ficha("O")
     print(f)
-    print(f.get_dueño())
+    print(f.get_jugador())
     print(f.get_capturada())
     f.set_capturada(True)
     print(f.get_capturada())
     print(f)
     print(g)    
+
 
         
