142672e452d782223fb87977c6ddb3ebcda965c1
Luchito-xx <luchoactis16@gmail.com>
2025-09-16 19:09:16 -0300

    Cambio minimo: class player -> class Player

--- core/player.py
+++ core/player.py
@@ -1,14 +1,14 @@
-class player:
+class Player:
     def __init__(self, name):
-        self.__name__ = name
-        self.__fichas_afuera__ = 0 # falta implementar
+        self.__name = name
+        self.__fichas_afuera = 0 # falta implementar
     
     def get_name(self):
-        return self.__name__
+        return self.__name
 
     def __repr__(self):
-        return f"Player: {self.__name__}"
+        return f"Player: {self.__name}"
     
 if __name__ == "__main__":
-    p = player("Lucho")
+    p = Player("Lucho")
     print(p)
