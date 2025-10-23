c7257d95d00148275bdaa704bb9e288f40466cd3
Luchito-xx <luchoactis16@gmail.com>
2025-10-23 01:01:48 -0300

    Documentadoc on docstrings de player

--- core/player.py
+++ core/player.py
@@ -1,12 +1,21 @@
 class Player:
+    """Clase que representa a un jugador en el juego.
+    Atributos:
+        __name : str
+            Nombre del jugador
+    """
     def __init__(self, name):
         self.__name = name
     
     def get_name(self):
+        """Obtener el nombre del jugador"""
         return self.__name
-
-    def __repr__(self):
-        return f"Player: {self.__name}"
     
     def set_name(self, name):
+        """Establecer el nombre del jugador"""
         self.__name = name
+
+    def __repr__(self):
+        """Representacion del jugador"""
+        return f"Player: {self.__name}"
+    
