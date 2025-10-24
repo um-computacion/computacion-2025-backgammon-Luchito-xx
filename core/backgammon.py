f124ae53a1bfa16d9cc3d5c70bf3fc9dcdae027b
Luchito-xx <luchoactis16@gmail.com>
2025-10-23 23:40:09 -0300

    Añadido de seleccion turno inicial, restructurado de llamadas a validaciones por cambios y comentado de mover()

--- core/backgammon.py
+++ core/backgammon.py
@@ -2,6 +2,7 @@ from .board import Board
 from .player import Player
 from .dice import Dice
 from .validaciones import *
+from .exceptions import *
 
 class Backgammon:
     """Clase principal del juego de Backgammon.
@@ -23,15 +24,35 @@ class Backgammon:
         self.__board = board if board else Board()
         self.__players = players if players else [Player("X"), Player("O")]
         self.__dice = dice if dice else Dice()
-        self.__turno = 0
+        self.__turno = None
         self.__saltos = []
         self.__ganador = None
 
     def inicio(self):
         """Iniciar el juego de Backgammon"""
         self.__board.inicio()
-        self.__turno = 0
+        self.__turno = None
         self.__saltos = []
+        self.__ganador = None
+
+        print("Seleccion de jugador inicial...")
+        self.turno_inicial()
+    
+    def turno_inicial(self):
+        """Determinar el primer jugador que inicia el juego"""
+        while True:
+            valores = self.__dice.roll()
+            print(f"Dados lanzados: {valores[0]} y {valores[1]}")
+            if valores[0] != valores[1]:
+                break
+            print("Empate, lanzo denuevo")
+        
+        if valores[0] > valores[1]:
+            self.__turno = 0
+        else:
+            self.__turno = 1
+        
+        print(f"El jugador {self.get_jugador().get_name()} juego primero")
     
     def tirar_dado(self):
         """Tirar los dados y obtener los saltos disponibles"""
@@ -45,31 +66,43 @@ class Backgammon:
     def get_jugador(self):
         """Obtener el jugador actual"""
         return self.__players[self.__turno]
+    
+    def mover(self, celda, salto):
+        """ Realizar un movimiento en el juego de Backgammon """
 
-    def mover(self, celda:int, salto:int):
-        """Mover una ficha en el tablero"""
+        # validar estado previo
         if not self.__saltos:
-            raise ValueError("Tirar dados primero")
+            raise SaltosError("Debes tirar dados primero")
         
-        try:
-            i = self.__saltos.index(salto)
-        except ValueError:
-            raise ValueError(f"No hay un salto pendiente igual a {salto}")
-
-        self.__saltos.pop(i)
-
-        jugador = self.get_jugador()
-
-        self.__board.mover(celda, salto, jugador.get_name())
-
-
-        if Validaciones.validar_victoria(self.__board.get_celdas(),self.__board.get_capturas(), jugador): #?? porque jugador no es un str
-            self.__ganador = jugador
-
+        if salto not in self.__saltos:
+            raise SaltosError(f"Salto {salto} no disponible")
+        
+        # validar desde validaciones movimiento
+        if not Validaciones.movimiento_valido(
+            celdas=self.__board.get_celdas(),
+            capturas=self.__board.get_capturas(),
+            celda=celda,
+            salto=salto,
+            jugador=self.get_jugador().get_name()
+        ):
+            raise ValueError("Movimiento inválido según las reglas")
+        
+        # mover desde board
+        self.__board.mover(celda, salto, self.get_jugador().get_name())
+        
+        # eliminar salto usado
+        self.__saltos.remove(salto)
+        
+        # ver si victoria
+        if Validaciones.validar_victoria(
+            self.__board.get_celdas(), 
+            self.__board.get_capturas(), 
+            self.get_jugador().get_name()):
+            self.__ganador = self.get_jugador()
+        
+        # cambiar turno si no hay mas saltos
         if not self.__saltos:
             self.cambio_turno()
-    
-
 
     def cambio_turno(self):
         """Cambiar el turno al siguiente jugador"""
@@ -83,6 +116,6 @@ class Backgammon:
             "board": self.__board.get_board(),
             "turno": self.get_jugador().get_name(),
             "saltos": list(self.__saltos),
-            "capturas":[(ficha.get_jugador() for ficha in self.__board.get_capturas())],
-            "ganador": self.__ganador
+            "capturas":list(self.__board.get_capturas()),
+            "ganador": self.__ganador.get_name() if self.__ganador else None
         }
