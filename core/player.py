ffb09ee1d5a969129dc8f45eeab7116c7fbd1934
Luchito-xx <luchoactis16@gmail.com>
2025-10-30 21:06:37 -0300

    Espacios corregidos

--- core/player.py
+++ core/player.py
@@ -1,8 +1,8 @@
 """
 Módulo Player - Representación de jugadores.
 
-Contiene la clase Player que representa a cada jugador en el juego
-de Backgammon con su identificador único.
+Un jugador se identifica por su identificador y participa en el juego
+realizando movimientos con sus fichas.
 """
 
 class Player:
@@ -15,7 +15,7 @@ class Player:
     """
     def __init__(self, name):
         """        
-            Inicializa un nuevo jugador con su identificador.
+        Inicializa un nuevo jugador con su identificador.
         Args:
             name (str): Identificador del jugador ('X' o 'O').
         """
@@ -23,7 +23,7 @@ class Player:
     
     def get_name(self):
         """
-            Obtiene el identificador del jugador.
+        Obtiene el identificador del jugador.
         Returns:
             str: Nombre/identificador del jugador.
         """
@@ -31,8 +31,9 @@ class Player:
     
     def set_name(self, name):
         """
-            Establece un nuevo identificador para el jugador.
+        Establece un nuevo identificador para el jugador.
         Args:
             name (str): Nuevo identificador del jugador.
         """
         self.__name__ = name
+        
